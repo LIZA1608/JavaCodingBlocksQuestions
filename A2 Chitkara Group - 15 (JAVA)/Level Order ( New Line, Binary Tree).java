8a67da9c4b344aba2b322aae20067f2ebdda3079
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-01-07 01:21:19 +0530

    Create Level Order ( New Line, Binary Tree).java

---
+++ A2 Chitkara Group - 15 (JAVA)/Level Order ( New Line, Binary Tree).java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Main {
+
+	static Scanner scn = new Scanner(System.in);
+	public static void main(String[] args) {
+		Main m = new Main();
+		BinaryTree bt = m.new BinaryTree();
+		bt.levelOrderZZ();
+	}
+
+	private class BinaryTree {
+		private class Node {
+			int data;
+			Node left;
+			Node right;
+		}
+		private Node root;
+		private int size;
+		public BinaryTree() {
+			this.root = this.takeInput(null, false);
+		}
+		public Node takeInput(Node parent, boolean ilc) {
+			int cdata = scn.nextInt();
+			Node child = new Node();
+			child.data = cdata;
+			this.size++;
+			// left
+			boolean hlc = scn.nextBoolean();
+			if (hlc) {
+				child.left = this.takeInput(child, true);
+			}
+			// right
+			boolean hrc = scn.nextBoolean();
+			if (hrc) {
+				child.right = this.takeInput(child, false);
+			}
+			// return
+			return child;
+		}
+		public void levelOrderZZ() {
+			// write your code here
+        Queue<Node> q
+            = new LinkedList<>();
+			q.add(root);
+			int flag=0;
+			while(!q.isEmpty()){
+				int size=q.size();
+				ArrayList<Integer>a=new ArrayList<>();
+					for(int i=0;i<size;i++){
+					Node front=q.peek();
+					a.add(front.data);
+					q.remove();
+					if(front.left!=null){
+						q.add(front.left);
+					}
+					if(front.right!=null){
+						q.add(front.right);
+					}
+					}
+						for(int i=0;i<a.size();i++){
+							System.out.print(a.get(i)+" ");
+						}
+					System.out.println();
+				}
+			}	
+}
+}
