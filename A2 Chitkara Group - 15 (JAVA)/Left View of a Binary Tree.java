5f6a062b5f43613cfa6d5230f0d991f3328ea995
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-01-07 01:32:51 +0530

    Create Left View of a Binary Tree.java

---
+++ A2 Chitkara Group - 15 (JAVA)/Left View of a Binary Tree.java
@@ -0,0 +1,81 @@
+mport java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.*;
+
+class Main {
+	static ArrayList<Integer>a=new ArrayList<>();
+	static class BinaryTreeFromLvlOrder {
+
+		 Scanner s = new Scanner(System.in);
+
+		 class Node {
+			int val;
+			Node left;
+			Node right;
+
+			public Node(int val) {
+				this.val = val;
+			}
+		}
+
+		 Node root;
+
+		public BinaryTreeFromLvlOrder() {
+			// TODO Auto-generated constructor stub
+			root = construct();// 2k
+		}
+
+		public Node construct() {
+			// TODO Auto-generated method stub
+
+			int val = s.nextInt();
+			Node nn = new Node(val);
+			Queue<Node> lvl = new LinkedList<>();
+			lvl.add(nn);
+
+			while (!lvl.isEmpty()) {
+				Node front = lvl.remove();
+				int a = s.nextInt();
+				int b = s.nextInt();
+
+				if (a != -1) {
+					Node na = new Node(a);
+					front.left = na;
+					lvl.add(na);
+				}
+
+				if (b != -1) {
+					Node nb = new Node(b);
+					front.right = nb;
+					lvl.add(nb);
+				}
+			}
+			return nn;// 2k
+		}
+		
+
+		//Write your function here
+		void solve(Node root,int count){
+		if(root==null){
+			return;
+		}
+		if(a.size()==count){
+          System.out.print(root.val+" ");
+		  a.add(root.val);
+		}
+		solve(root.left,count+1);
+		solve(root.right,count+1);
+	}
+	}
+    
+	public static void main(String[] args) {
+		
+		BinaryTreeFromLvlOrder bt = new BinaryTreeFromLvlOrder();
+		
+		
+		bt.solve(bt.root,0);
+
+		
+	}
+}
