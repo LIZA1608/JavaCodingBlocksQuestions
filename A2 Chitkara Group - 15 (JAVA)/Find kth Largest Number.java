2cfd66b4d3ad8bb62c718705c342472e4af99274
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-01-08 21:54:07 +0530

    Update Find kth Largest Number.java

--- A2 Chitkara Group - 15 (JAVA)/Find kth Largest Number.java
+++ A2 Chitkara Group - 15 (JAVA)/Find kth Largest Number.java
@@ -2,21 +2,28 @@
 
 import java.util.*;
 public class Main {
-    public static int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+    public static int findklargest(int arr[],int k,int n){
+        PriorityQueue<Integer>pq=new PriorityQueue<Integer>();
+        for(int i=0;i<k;i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k;i<n;i++){
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        int ans=pq.peek();
+        return ans;
     }
-    public static void main (String args[]) {
-       
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int k=sc.nextInt();
-        int []arr=new int[n];
+        int arr[]=new int [n];
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+              arr[i]=sc.nextInt();
         }
-           
-        int kthLargest = findKthLargest(arr, k);
-        System.out.println(kthLargest);
+        int ans=findklargest(arr,k,n);
+        System.out.print(ans);
     }
-}
